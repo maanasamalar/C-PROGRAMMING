4d0fcab44d19f3ae1a4998a7780db8e5dd4c43aa
maanasamalar <15eucs067@skcet.ac.in>
2017-12-18 15:06:52 +0530

    Create player.c

---
+++ Beginner level/player.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char name[50]="Sachin Ramesh Tendulkar";
+char nik[50]="Little Master";
+int runs=18426;
+float avg=44.83;
+int match=463;
+printf("%s\n",name);
+printf("%s\n",nik);
+printf("%d\n",runs);
+printf("%0.2f\n",avg);
+printf("%d\n",match);
+return 0;
+}
