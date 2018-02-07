bae594088679a71d1841240871fe72b8125fe583
maanasamalar <15eucs067@skcet.ac.in>
2018-02-07 20:33:59 +0530

    Create max_in_array.c

---
+++ Code Kata/Beginner/max_in_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[50];
+    scanf("%d",&n);
+    int i,max;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+        
+        if(max<a[i])
+        {
+            max=a[i];
+           // break;
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
