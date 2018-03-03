a584024a91b3f59f4116142e0aa4c626f888dbd5
maanasamalar <15eucs067@skcet.ac.in>
2018-03-03 20:45:10 +0530

    Create count_no_of_lines.c

---
+++ Code Kata/Beginner/count_no_of_lines.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int len,i,count=1;
+    char str[50];
+  scanf("%[^\n]s",str);
+    len=strlen(str);
+    for(i=0;i<len;i++){
+       /* count++;
+    }
+    for(i=0;i<len;i++)
+    {*/
+        if(str[i]=='.')
+        {
+            count=count+1;
+        }
+    }
+    printf("no of lines:%d",count);
+    return 0;
+}
