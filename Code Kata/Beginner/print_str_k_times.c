b4cfd5a30f1114bd8451582155b6862d0c28de2e
maanasamalar <15eucs067@skcet.ac.in>
2018-03-14 13:02:15 +0530

    Create print_str_k_times.c

---
+++ Code Kata/Beginner/print_str_k_times.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[50];
+    int i,k;
+    scanf("%s",str);
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        printf("%s ",str);
+    }
+    
+    return 0;
+    
+}
